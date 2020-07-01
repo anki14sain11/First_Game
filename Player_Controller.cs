b49fb2ec3b7a54e33634f3294fe94e9074d548d4
anki14sain11 <67688931+anki14sain11@users.noreply.github.com>
2020-07-01 16:34:51 +0530

    Add files via upload

---
+++ Player_Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Player_motor))]
+public class Player_Controller : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    private Player_motor motor;
+    void Start()
+    {
+        motor = GetComponent<Player_motor>();
+    }
+    void update()
+    {
+        float xMov = Input.GetAxisRaw("Horizontal"); 
+        float zMov = Input.GetAxisRaw("Vertical");
+        Vector3 movHorizontal = transform.right*xMov;
+        Vector3 movVertical = transform.forward*zMov;
+
+        Vector3 velocity = (movHorizontal + movVertical).normalized*speed;
+        motor.Move(velocity);
+        
+    }
+}
---
+++ Player_motor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player_motor : MonoBehaviour
+{
+    private Vector3 vel = Vector3.zero;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+    public void Move(Vector3 velocity)
+    {
+        vel = velocity;
+    }
+    void FixedUpdate()
+    {
+        PerformMovement();
+    }
+    void PerformMovement()
+    {
+        if (vel != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + vel*Time.fixedDeltaTime);
+        }
+    }
+}
